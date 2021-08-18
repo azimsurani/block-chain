4af6874804f8f4be15440377231321dc9aebfa61
azimsurani <azimsurani786@gmail.com>
2021-08-18 00:21:01 -0400

    Added Block Chain Mining Route

--- blockchain.py
+++ blockchain.py
@@ -63,9 +63,28 @@ class Blockchain:
             previous_block = block
         
         return True
-        
-        
-        
-
     
+
 # Module 2 - Mining  our Blockchain
+
+# Creating a flask app
+app = Flask(__name__)
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
+@app.route("/mine_block", methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    previous_hash = blockchain.hash(previous_block)
+    proof = blockchain.proof_of_work(previous_proof)
+    block = blockchain.create_block(proof, previous_hash)
+    response = { "Message" : "Congratulations, you just mined a block",
+                "index":block["index"],
+                "timestamp":block["timestamp"],
+                "proof":block["proof"],
+                "previous_hash":block["previous_hash"]}
+    
+    return jsonify(response), 200
+    
