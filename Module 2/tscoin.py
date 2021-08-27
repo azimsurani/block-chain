91aacd8be6ae7cc1791f5f52fbb95d548ef6654f
azimsurani <azimsurani786@gmail.com>
2021-08-27 16:10:48 -0400

    Added Transactions

--- Module 2/tscoin.py
+++ Module 2/tscoin.py
@@ -15,10 +15,13 @@ import requests
 from uuid import uuid4
 from urllib.parse import urlparse
 
+# Part 1 - Building a blockchain
+
 class Blockchain:
     
     def __init__(self):
         self.chain = []
+        self.transactions = []
         self.create_block(proof=1,previous_hash='0')
     
     def create_block(self,proof,previous_hash):
@@ -26,8 +29,10 @@ class Blockchain:
                     'index': len(self.chain)+1,
                     'timestamp' : str(datetime.datetime.now()),
                     'proof':proof,
-                    'previous_hash':previous_hash
+                    'previous_hash':previous_hash,
+                    'transactions': self.transactions
                 }
+        self.transactions = []
         self.chain.append(block)
         return block
     
@@ -67,8 +72,18 @@ class Blockchain:
         
         return True
     
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({
+                'sender': sender,
+                'receiver': receiver,
+                'amount': amount
+            })
+        
+        previous_block = self.get_previous_block()
+        return previous_block['index']+1
+    
 
-# Module 2 - Mining  our Blockchain
+# Part 2 - Mining  our Blockchain
 
 # Creating a flask app
 app = Flask(__name__)
